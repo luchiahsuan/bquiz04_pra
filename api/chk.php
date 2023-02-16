1d687ead2cfad3feeec722243f6714d1fa731450
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-16 12:43:56 +0800

    建立檢測帳號的功能

---
+++ api/chk.php
@@ -0,0 +1,12 @@
+<?php
+include_once "base.php";
+$Mem->count(['acc'=>$_GET['acc']]);
+if($chk>0){
+    echo 1;
+}else{
+    echo 0;
+}
+
+
+
+?>
--- back/mem_detail.php
+++ back/mem_detail.php
@@ -4,27 +4,27 @@
 <table class="all">
     <tr>
         <td class="tt ct">姓名</td>
-        <td class="pp"><input type="text" name="name" id=""></td>
+        <td class="pp"><input type="text" name="name" id="name"></td>
     </tr>
     <tr>
         <td class="tt ct">帳號</td>
-        <td class="pp"><input type="text" name="acc" id=""></td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
     </tr>
     <tr>
         <td class="tt ct">密碼</td>
-        <td class="pp"><input type="password" name="pw" id=""></td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
     </tr>
     <tr>
         <td class="tt ct">電話</td>
-        <td class="pp"><input type="text" name="tel" id=""></td>
+        <td class="pp"><input type="text" name="tel" id="tel"></td>
     </tr>
     <tr>
         <td class="tt ct">住址</td>
-        <td class="pp"><input type="text" name="addr" id=""></td>
+        <td class="pp"><input type="text" name="addr" id="addr"></td>
     </tr>
     <tr>
         <td class="tt ct">電子信箱</td>
-        <td class="pp"><input type="text" name="email" id=""></td>
+        <td class="pp"><input type="text" name="email" id="email"></td>
     </tr>
 </table>
 <div class="ct">
