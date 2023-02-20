6786f14a6009bd88d2b85934a9042fc0f444f189
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-20 14:42:07 +0800

    在購物車頁面顯示使用者選購的商品列表

--- front/buycart.php
+++ front/buycart.php
@@ -1,12 +1,47 @@
 <?php
-if(isset($_GET['id'])){
-    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+if (isset($_GET['id'])) {
+    $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
 }
 
-if(isset($_SESSION['mem'])){
-}else{
+if (isset($_SESSION['mem'])) {
+} else {
     to("index.php?do=login");
 }
 
-dd($_SESSION['cart']);
+// dd($_SESSION['cart']);
+?>
+<h2 class="ct"><?= $_SESSION['mem']; ?>的購物車</h2>
 
+<table class="all">
+    <tr class="tt ct">
+        <td class="pp">編號</td>
+        <td class="pp">商品名稱</td>
+        <td class="pp">數量</td>
+        <td class="pp">庫存量</td>
+        <td class="pp">單價</td>
+        <td class="pp">小計</td>
+        <td class="pp">刪除</td>
+    </tr>
+    <?php
+    foreach ($_SESSION['cart'] as $id => $qt) {
+        $row = $Goods->find($id);
+    ?>
+        <tr class="pp">
+            <td class="pp"><?= $row['no']; ?></td>
+            <td class="pp"><?= $row['name']; ?></td>
+            <td class="pp"><?= $qt; ?></td>
+            <td class="pp"><?= $row['stock']; ?></td>
+            <td class="pp"><?= $row['price']; ?></td>
+            <td class="pp"><?= $row['price'] * $qt; ?></td>
+            <td class="pp">
+                <img src="./icon/0415.jpg" alt="">
+            </td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
+<div class="ct">
+    <img src="./icon/0411.jpg" alt="">
+    <img src="./icon/0412.jpg" alt="">
+</div>
