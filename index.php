cba500be337348039089a6a6d114290f760dbc58
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-20 13:43:24 +0800

    在buycart判斷使用者是否為登入會員並導向購物車或登入頁

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php include_once './api/base.php'; ?>
+<?php include_once './api/base.php';
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -93,6 +94,7 @@
 
 
                         ?>
+
 
                 </div>
                 <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
