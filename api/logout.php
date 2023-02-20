b491359c4b33633220a713ecb2e50376c2122e59
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-20 15:30:40 +0800

    增加判斷使用者沒有點商品就到購物車的情形

--- api/logout.php
+++ api/logout.php
@@ -1,6 +1,7 @@
 <?php include_once "base.php";
 
 unset($_SESSION[$_GET['table']]);
+unset($_SESSION['cart']);
 
 to('../index.php');
 
--- front/buycart.php
+++ front/buycart.php
@@ -11,37 +11,44 @@ if (isset($_SESSION['mem'])) {
 // dd($_SESSION['cart']);
 ?>
 <h2 class="ct"><?= $_SESSION['mem']; ?>的購物車</h2>
-
-<table class="all">
-    <tr class="tt ct">
-        <td class="pp">編號</td>
-        <td class="pp">商品名稱</td>
-        <td class="pp">數量</td>
-        <td class="pp">庫存量</td>
-        <td class="pp">單價</td>
-        <td class="pp">小計</td>
-        <td class="pp">刪除</td>
-    </tr>
-    <?php
-    foreach ($_SESSION['cart'] as $id => $qt) {
-        $row = $Goods->find($id);
-    ?>
-        <tr class="pp">
-            <td class="pp"><?= $row['no']; ?></td>
-            <td class="pp"><?= $row['name']; ?></td>
-            <td class="pp"><?= $qt; ?></td>
-            <td class="pp"><?= $row['stock']; ?></td>
-            <td class="pp"><?= $row['price']; ?></td>
-            <td class="pp"><?= $row['price'] * $qt; ?></td>
-            <td class="pp">
-                <img src="./icon/0415.jpg" alt="">
-            </td>
+<?php
+if (!isset($_SESSION['cart'])) {
+    echo "<h2 class='ct'> 目前購物車沒有商品 </h2>";
+} else {
+?>
+    <table class="all">
+        <tr class="tt ct">
+            <td class="pp">編號</td>
+            <td class="pp">商品名稱</td>
+            <td class="pp">數量</td>
+            <td class="pp">庫存量</td>
+            <td class="pp">單價</td>
+            <td class="pp">小計</td>
+            <td class="pp">刪除</td>
         </tr>
-    <?php
-    }
-    ?>
-</table>
-<div class="ct">
-    <img src="./icon/0411.jpg" alt="">
-    <img src="./icon/0412.jpg" alt="">
-</div>
+        <?php
+        foreach ($_SESSION['cart'] as $id => $qt) {
+            $row = $Goods->find($id);
+        ?>
+            <tr class="pp">
+                <td class="pp"><?= $row['no']; ?></td>
+                <td class="pp"><?= $row['name']; ?></td>
+                <td class="pp"><?= $qt; ?></td>
+                <td class="pp"><?= $row['stock']; ?></td>
+                <td class="pp"><?= $row['price']; ?></td>
+                <td class="pp"><?= $row['price'] * $qt; ?></td>
+                <td class="pp">
+                    <img src="./icon/0415.jpg" alt="">
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <div class="ct">
+        <img src="./icon/0411.jpg">
+        <img src="./icon/0412.jpg">
+    </div>
+<?php
+}
+?>
