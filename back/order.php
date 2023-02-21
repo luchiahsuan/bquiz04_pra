42b0ef90bde495c1f1323d4d92a5e420801ced26
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-21 08:35:50 +0800

    建立訂單管理畫面

---
+++ back/order.php
@@ -0,0 +1,19 @@
+<h2 class="ct">訂單管理</h2>
+<table class="all">
+    <tr class="ct tt">
+        <td>訂單編號</td>
+        <td>金額</td>
+        <td>會員帳號</td>
+        <td>姓名</td>
+        <td>下單時間</td>
+        <td>操作</td>
+    </tr>
+    <tr class="ct pp">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
